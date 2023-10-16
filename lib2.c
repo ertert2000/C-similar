d8a2abb1cd68244146bcd60a0a9a2ec6ad6ec7b2
Timokrut <potapov.tim14@gmail.com>
2023-10-16 19:21:13 +0300

    upgrade mindset

--- lib2.c
+++ lib2.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
 #include "func.h"
-#define N 9
 
 int main()
 {
-    int m,k,s;
+    int N, m, k, s;
+    scanf("%d", &N);
     int A[N];
-    int i,temp=0,j;
+    int i, temp = 0, j;
 
+    puts("enter array");
+    for (j = 0; j < N; j++)
+        scanf_s("%d", &A[j]);
 
-    puts("enter arrey");
-    for (j=0;j<9;j++)
-        scanf_s("%d",&A[j]);
     puts("kolicheestvo elementov kotoroe hotite perestavit");
-    scanf_s("%d",&m);
+    scanf_s("%d", &m);
+    
     puts("enter nomer elementa s rjnjhuj hotite nachat perestanovku");
-    scanf_s("%d",&k);
+    scanf_s("%d", &k);
+    
     puts("element kuda hotite perestavit");
-    scanf_s("%d",&s);
+    scanf_s("%d", &s);
 
     size_t n = sizeof(A) / sizeof(int);
 
-    for (i=k;i<k+m;i++)
+    for (i = k; i < k + m; i++)
     {
-        temp=A[s];
-        A[s]=A[i];
-        A[i]=temp;
+        temp = A[s];
+        A[s] = A[i];
+        A[i] = temp;
         s++;
     }
 
     puts("");
-    printmas(A,n);
+    printmas(A, n);
 
     return 0;
 }
