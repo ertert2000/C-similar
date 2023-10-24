4213cf476984559d5379080a266ee866c0baaf5e
ertert2000 <ertert2000@yandex.ru>
2023-10-24 17:20:24 +0300

    update

--- lib2/lib2/lib2/lib2.c
+++ lib2/lib2/lib2/lib2.c
@@ -1,60 +1,49 @@
 #include <stdio.h>
-#include "func.h"
 #define N 9
+
+void printmas(int M[], int n)
+{
+    int j;
+    for (j = 0; j < n; j++)
+        printf("%d\t", M[j]);
+}
 
 int main()
 {
-    int m,k,s;
+    int m, k, s;
+    int i, temp = 0, j;
     int A[N];
-    int i,temp=0,j;
 
 
-    puts("enter arrey");
-    for (j=0;j<9;j++)
-        scanf_s("%d",&A[j]);
-    puts("kolicheestvo elementov kotoroe hotite perestavit");
-    scanf_s("%d",&m);
-    puts("enter nomer elementa s rjnjhuj hotite nachat perestanovku");
-    scanf_s("%d",&k);
-    puts("element kuda hotite perestavit");
-    scanf_s("%d",&s);
+    puts("enter array");
+    for (j = 0; j < N; j++)
+        scanf_s("%d", &A[j]);
+
+    puts("how many elements do you want to rearrange");
+    scanf_s("%d", &m);
+
+    puts("enter the number of the element you want to start the permutation from");
+    scanf_s("%d", &k);
+
+    puts("enter the number of the position where you want to rearrange the group of elements");
+    scanf_s("%d", &s);
 
 
+    size_t n = sizeof(A) / sizeof(int);
 
-    for (i=k;i<k+m;i++)
+
+    for (i = k; i < k + m; i++)
     {
-        temp=A[s];
-        A[s]=A[i];
-        A[i]=temp;
+        temp = A[s];
+        A[s] = A[i];
+        A[i] = temp;
         s++;
     }
 
+
     puts("");
-    printmas(A);
+    printmas(A, n);
+
 
     return 0;
 }
-
-/*#include <stdio.h>
-
-
-int main()
-{
-    int m=3,k=5,s=1;
-    int A[9]={1,2,3,4,5,6,7,8,9};
-    int i,kech=0,j;
-
-    for (i=k;i<k+m;i++)
-    {
-        kech=A[s];
-        A[s]=A[i];
-        A[i]=kech;
-        s++;
-    }
-
-    for (j=0;j<9;j++)
-        printf("%d\n",A[j]);
-
-
-    return 0;
-}*/
