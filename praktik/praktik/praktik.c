30f10eba35e87d9098bf52270a1741618861af07
ertert2000 <ertert2000@yandex.ru>
2023-11-19 19:55:16 +0300

    #df

--- praktik/praktik/praktik.c
+++ praktik/praktik/praktik.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include "func.h"
+#include <math.h>
 
+int matrix(int matrix[][64], int arr[], int n)
+{
+    int k = cell(sqrt);
+    int i, j;
+    int index = 0;
+
+    for (int i = 0; i < 64; i++)
+        for (int j = 0; j < 64; j++)
+        {
+            if (index < n)
+            {
+                matrix[i][j] = arr[index];
+                index++;
+            }
+            else
+                matrix[i][j] = 0;
+        }
+    return 0;
+}
 
 
 int main()
