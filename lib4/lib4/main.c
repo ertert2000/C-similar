2df0979b89af8176efd950cb260aeca236060c19
ertert2000 <ertert2000@yandex.ru>
2023-11-27 00:18:30 +0300

    ebotny

---
+++ lib4/lib4/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#define Mrows 10
+#define Mcols 10
+
+void inputArray(int r, int c, int arr[Mrows][Mcols]) {
+	printf("Enter array elements:\n");
+
+	for (int i = 0; i < r; i++)
+		for (int j = 0; j < c; j++)
+			scanf_s("%d", &arr[i][j]);
+	for (int i = 0; i < r; i++)
+	{
+		int max = 0;
+		for (int j = 0; j < c; j++)
+		{
+			if (arr[i][j] < 0)
+			{
+				for (int p = 0; p < c; p++)
+					if (max < arr[i][p])
+					{
+						max = arr[i][p];
+					}
+				for (int k = j; k < c; k++)
+					arr[i][k + 1] = max;
+			}
+		}
+	}
+}
+
+
+int isColumnOdd(int rows, int col, int arr[Mrows][Mcols], int colIndex) {
+	for (int i = 0; i < rows; ++i) {
+		if (arr[i][colIndex] % 2 == 0) {
+			return 0;
+		}
+	}
+	return 1;  
+}
+
+void deleteOddColumns(int rows, int* cols, int arr[Mrows][Mcols]) {
+	int newCols = *cols;
+
+	for (int j = 0; j < *cols; ++j) {
+		if (isColumnOdd(rows, *cols, arr, j)) {
+			for (int i = 0; i < rows; ++i)
+				for (int k = j; k < *cols - 1; ++k) 
+					arr[i][k] = arr[i][k + 1];
+
+			--newCols;
+			--j;        
+		}
+	}
+
+	*cols = newCols;
+}
+
+void printArray(int rows, int cols, int arr[Mrows][Mcols]) {
+	printf("Converted array:\n");
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j)
+			printf("%d\t", arr[i][j]);
+		printf("\n");
+	}
+}
+
+int main() {
+	int rows=11, cols=11;
+
+	printf("Enter the number of rows and columns not exceeding 10:");
+	while (Mrows < rows)
+		scanf_s("%d", &rows);
+	while (Mcols < cols)
+		scanf_s("%d", &cols);
+
+	int arr[Mrows][Mcols];
+
+	inputArray(rows, cols, arr);
+	deleteOddColumns(rows, &cols, arr);
+	printArray(rows, cols, arr);
+
+	return 0;
+}
--- praktik/praktik/praktik.c
+++ praktik/praktik/praktik.c
@@ -1,33 +1,107 @@
 #include <stdio.h>
-#include "func.h"
-#include <math.h>
+#include <locale.h>
 
-int matrix(int matrix[][64], int arr[], int n)
-{
-    int k = cell(sqrt);
-    int i, j;
-    int index = 0;
-
-    for (int i = 0; i < 64; i++)
-        for (int j = 0; j < 64; j++)
-        {
-            if (index < n)
-            {
-                matrix[i][j] = arr[index];
-                index++;
-            }
-            else
-                matrix[i][j] = 0;
+// Функция для ввода строки массива
+void inputRow(int* row, int size) {
+    for (int i = 0; i < size; ++i) {
+        scanf_s("%d", &row[i]);
+        if (row[i] < 0) {
+            break;
         }
-    return 0;
+    }
 }
 
+// Функция для вывода строки массива
+void printRow(int* row, int size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", row[i]);
+    }
+    printf("\n");
+}
 
-int main()
-{
-    int a;
-    a = 2500 - (690 * 3);
-    printf("%d",a);
+// Функция для определения количества четных чисел в столбце
+int countEvenInColumn(int** array, int rows, int colIndex, int evenCount) {
+    int count = 0;
+    for (int i = 0; i < rows; ++i) {
+        if (array[i][colIndex] % 2 == 0) {
+            count++;
+        }
+    }
+    return (count == evenCount) ? 1 : 0;
+}
+
+// Функция для формирования результирующего массива
+void generateResultArray(int** array, int rows, int cols, int evenCount) {
+    printf("\nИсходный массив:\n");
+    for (int i = 0; i < rows; ++i) {
+        printRow(array[i], cols);
+    }
+
+    printf("\nСтолбцы с %d четными числами:\n", evenCount);
+    for (int j = 0; j < cols; ++j) {
+        if (countEvenInColumn(array, rows, j, evenCount)) {
+            for (int i = 0; i < rows; ++i) {
+                printf("%d ", array[i][j]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int rows;
+
+    // Вводим количество строк
+    printf("Введите количество строк: ");
+    scanf_s("%d", &rows);
+
+    // Выделяем память под двумерный массив
+    int** array = (int**)malloc(rows * sizeof(int*));
+    if (array == NULL) {
+        printf("Ошибка выделения памяти.\n");
+        return 1;
+    }
+
+    // Вводим элементы массива
+    printf("Введите элементы массива. Завершите строку отрицательным числом:\n");
+    int size = 0;
+    for (int i = 0; i < rows; ++i) {
+        array[i] = (int*)malloc(sizeof(int));
+        if (array[i] == NULL) {
+            printf("Ошибка выделения памяти.\n");
+            return 1;
+        }
+
+        inputRow(array[i], size);
+        if (i == 0) {
+            // Определение размера строки для последующих строк
+            while (array[i][size] >= 0) {
+                size++;
+                array[i] = realloc(array[i], (size + 1) * sizeof(int));
+                if (array[i] == NULL) {
+                    printf("Ошибка выделения памяти.\n");
+                    return 1;
+                }
+                scanf_s("%d", &array[i][size]);
+            }
+        }
+    }
+
+    // Вводим количество четных чисел в столбце для формирования результирующего массива
+    int evenCount;
+    printf("Введите количество четных чисел в столбце: ");
+    scanf_s("%d", &evenCount);
+
+    // Формируем результирующий массив и выводим результат
+    generateResultArray(array, rows, size, evenCount);
+
+    // Освобождаем выделенную память
+    for (int i = 0; i < rows; ++i) {
+        free(array[i]);
+    }
+    free(array);
+
     return 0;
 }
 
