9df5cb3912da1a0741e2b23edffa98a8d24fd246
ertert2000 <ertert2000@yandex.ru>
2023-12-07 20:57:25 +0300

    fileeeeeeeeeee

--- lib5.1/lib5.1/lib5_1.c
+++ lib5.1/lib5.1/lib5_1.c
@@ -4,90 +4,56 @@
 #include <string.h>
 #include <locale.h>
 //C:\Users\vanya\Documents\teest.txt
-void outputINconsol()
-{
-}
+//C:\\Users\\»ван\\Documents\\teets.txt
+//F:\teets.txt
 
 int main()
 {
+	setlocale(LC_ALL, "RUS");
 	int ckl=10;
-	char string[1000];
-	char rasdeliteli[100];
+	int ASCLL[256] = { 0 };
 	int next;
 	int	next1;
-	int n = 0, r = 0;
+	int len;
+	char string[1000];
+	char rasdeliteli[100];
+	char wakefilerasd[500];
 	size_t endstring;
 	FILE* polsovat;
-	FILE* ras;
-	char wakefilestring[500];
-	char wakefilerasd[500];
-		
+	
 
-	scanf_s("%d", &next);
-	while ((getchar()) != '\n');
-
-	switch (next)
+	while (ckl < 11)
 	{
-	case 1:
-		system("cls");
-		
-		puts("Enter a text string consisting of words and an arbitrary number of delimiter characters after the words");
-		fgets(string, 1000, stdin);
+		puts("1 - determining the number of delimiter characters with manual line input\n2 - determining the number of delimiter characters with file input\n3 - to complete the program");
 
-		puts("Enter the characters you want to act as delimiters");
-		fgets(rasdeliteli, 100, stdin);
-
-		size_t endstring = strlen(string);
-		if (string[endstring - 1] = '\n')
-			string[endstring - 1] = '\0';
-
-		int ASCLL[256] = { 0 };
-
-		for (int i = 0; i < strlen(rasdeliteli); i++)
-			ASCLL[rasdeliteli[i]] = 0;
-		for (int j = 0; j < strlen(string); j++)
-			if (strchr(rasdeliteli, string[j]) != NULL)
-				ASCLL[string[j]]++;
-
-		for (int i = 0; i < 256; i++)
-			if (ASCLL[i] != 0)
-				printf("'%c' Found: % d once\n", i, ASCLL[i]);
-		break;
-	case 2:
-		scanf_s("%d", &next1);
+		scanf_s("%d", &next);
 		while ((getchar()) != '\n');
-		switch (next1)
+
+		system("cls");
+
+		switch (next)
+		{
+		case 1:
+			puts("1 - English\n2 - Russian");
+			
+			scanf_s("%d", &next1);
+			while ((getchar()) != '\n');
+
+			switch (next1)
 			{
 			case 1:
-				puts("");
-				fgets(wakefilerasd, 500, stdin);
-				int len = strlen(wakefilerasd);
-				if (len > 0 && wakefilerasd[len - 1] == '\n') 
-				{
-					wakefilerasd[len - 1] = '\0'; 
-				}
-				polsovat = fopen(wakefilerasd, "w");
-				
-				/*puts("");
-				fgets(wakefilerasd, 500, stdin);
-				ras = fopen(wakefilerasd, "w");*/
-				while (!feof(polsovat))
-				{
-					fgets(string[n], 1000, polsovat);
-					n++;
-				}
-				/*while (!feof(ras))
-				{
-					fgets(rasdeliteli[r], 1000, ras);
-					r++;
-				}*/
+				system("cls");
+
+				puts("Enter a text string consisting of words and an arbitrary number of delimiter characters after the words");
+				fgets(string, 1000, stdin);
+
+				puts("Enter the characters you want to act as delimiters");
 				fgets(rasdeliteli, 100, stdin);
+
 				endstring = strlen(string);
 				if (string[endstring - 1] = '\n')
 					string[endstring - 1] = '\0';
 
-				for (int i = 0; i < strlen(rasdeliteli); i++)
-					ASCLL[rasdeliteli[i]] = 0;
 				for (int j = 0; j < strlen(string); j++)
 					if (strchr(rasdeliteli, string[j]) != NULL)
 						ASCLL[string[j]]++;
@@ -95,56 +61,121 @@ int main()
 				for (int i = 0; i < 256; i++)
 					if (ASCLL[i] != 0)
 						printf("'%c' Found: % d once\n", i, ASCLL[i]);
-				fclose(polsovat);
-				//fclose(ras);
+
+				getchar();
+				system("cls");
 				break;
 			case 2:
+				system("cls");
+
+				puts("¬ведите текстовую строку, состо€щую из слов и произвольного количества символов-разделителей после слов");
+				fgets(string, 1000, stdin);
+
+				puts("¬ведите символы, которые вы хотите использовать в качестве разделителей.");
+				fgets(rasdeliteli, 100, stdin);
+
+				endstring = strlen(string);
+				if (string[endstring - 1] = '\n')
+					string[endstring - 1] = '\0';
+
+				for (int j = 0; j < strlen(string); j++)
+					if (strchr(rasdeliteli, string[j]) != NULL)
+						ASCLL[string[j]]++;
+
+				for (int i = 0; i < 256; i++)
+					if (ASCLL[i] != 0)
+						printf("'%c' найдено: % d раз\n", i, ASCLL[i]);
+
+				getchar();
+				system("cls");
 				break;
 			}
-		break;
-	case 3:
-		return 0;
-		break;
+			break;
+		case 2:
+			puts("1 - English\n2 - Russian");
+
+			scanf_s("%d", &next1);
+			while ((getchar()) != '\n');
+
+			switch (next1)
+			{
+			case 1:
+				system("cls");
+
+				puts("enter the path to the file");
+
+				fgets(wakefilerasd, 100, stdin);
+
+				len = strlen(wakefilerasd);
+				if (wakefilerasd[len - 1] = '\n')
+					wakefilerasd[len - 1] = '\0';
+
+				polsovat = fopen(wakefilerasd, "r");
+				if (polsovat == NULL)
+					return 1;
+
+				fgets(string, 1000, polsovat);
+
+				endstring = strlen(string);
+				if (string[endstring - 1] == '\n')
+					string[endstring - 1] = '\0';
+
+				puts("enter delimiter characters");
+				fgets(rasdeliteli, 100, stdin);
+
+				for (int j = 0; j < strlen(string); j++)
+					if (strchr(rasdeliteli, string[j]) != NULL)
+						ASCLL[string[j]]++;
+
+				for (int i = 0; i < 256; i++)
+					if (ASCLL[i] != 0)
+						printf("'%c' Found: % d once\n", i, ASCLL[i]);
+
+				fclose(polsovat);
+				getchar();
+				system("cls");
+				break;
+			case 2:
+				system("cls");
+
+				puts("введите путь к файлу");
+
+				fgets(wakefilerasd, 100, stdin);
+
+				len = strlen(wakefilerasd);
+				if (wakefilerasd[len - 1] = '\n')
+					wakefilerasd[len - 1] = '\0';
+
+				polsovat = fopen(wakefilerasd, "r");
+				if (polsovat == NULL)
+					return 1;
+
+				fgets(string, 1000, polsovat);
+
+				endstring = strlen(string);
+				if (string[endstring - 1] == '\n')
+					string[endstring - 1] = '\0';
+
+				puts("введите символы разделители");
+				fgets(rasdeliteli, 100, stdin);
+
+				for (int j = 0; j < strlen(string); j++)
+					if (strchr(rasdeliteli, string[j]) != NULL)
+						ASCLL[string[j]]++;
+
+				for (int i = 0; i < 256; i++)
+					if (ASCLL[i] != 0)
+						printf("'%c' Found: % d once\n", i, ASCLL[i]);
+
+				fclose(polsovat);
+				getchar();
+				system("cls");
+				break;
+			}
+			break;
+		case 3:
+			return 0;
+			break;
+		}
 	}
 }
-
-
-
-//switch (next1)
-			//{
-			//case 1:
-			//	puts("");
-			//	//fgets(wakefilerasd, 500, stdin);
-			//	polsovat = fopen(fgets(wakefilerasd, 500, stdin), "w");
-			//	puts("");
-			//	fgets(wakefilerasd, 500, stdin);
-			//	ras = fopen(wakefilerasd, "w");
-			//	while (!feof(polsovat))
-			//	{
-			//		fgets(string[n], 1000, polsovat);
-			//		n++;
-			//	}
-			//	while (!feof(ras))
-			//	{
-			//		fgets(rasdeliteli[r], 1000, ras);
-			//		r++;
-			//	}
-			//	endstring = strlen(string);
-			//	if (string[endstring - 1] = '\n')
-			//		string[endstring - 1] = '\0';
-
-			//	for (int i = 0; i < strlen(rasdeliteli); i++)
-			//		ASCLL[rasdeliteli[i]] = 0;
-			//	for (int j = 0; j < strlen(string); j++)
-			//		if (strchr(rasdeliteli, string[j]) != NULL)
-			//			ASCLL[string[j]]++;
-
-			//	for (int i = 0; i < 256; i++)
-			//		if (ASCLL[i] != 0)
-			//			printf("'%c' Found: % d once\n", i, ASCLL[i]);
-			//	fclose(polsovat);
-			//	fclose(ras);
-			//	break;
-			//case 2:
-			//	break;
-			//}
--- praktik/praktik/praktik.c
+++ praktik/praktik/praktik.c
@@ -1,8 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
 
 int main() {
-    for (int i = 0; i < 256; i++)
-        printf("%d: %c\n", i,i);
+    char put[100];
+    
+    fgets(put, 100, stdin);
+
+    int len = strlen(put);
+    if (put[len - 1] = '\n')
+        put[len - 1] = '\0';
+
+
+    /*int c;
+    int index=0;
+    while ((c = getchar()) != '\n' && index < 99)
+    {
+        put[index] = c;
+        index++;
+    }
+    put[index] = '\0';*/
+    
+    FILE* f = fopen(put, "r");
+    char string[100];
+
+    if (f == NULL)
+        return 1;
+    
+    fgets(string, 100, f);
+
+    printf("%s", string);
+
+    fclose(f);
     return 0;
 }
