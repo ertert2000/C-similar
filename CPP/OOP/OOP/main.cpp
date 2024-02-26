bee79cb237bda6e9d792bced74b91cabe8fbf5d5
vanya <ertert2000@yandex.ru>
2024-02-26 13:03:13 +0300

    endl

---
+++ CPP/OOP/OOP/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+class Person
+{
+public:
+	std::string name;
+	int age;
+	void print()
+	{
+		cout << "Name: " << name << "\tAge: " << age << endl;
+	}
+};
+
+
+int main()
+{
+	Person person;
+	Person *ptr = &person;
+	
+
+	person.name = "Alex";
+	person.age = 19;
+
+
+	std::string name = person.name; //8
+	int age = person.age;
+	person.print();
+
+	cout << "Name: " << name << "\tAge: " << age << endl;
+
+	//puts("");
+	ptr->name = "Tom";
+	ptr->age = 25;
+
+	ptr->print();
+
+	return 0;
+}
