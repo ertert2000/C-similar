1ee6cbcdafededf26b4c7688894417cc302b2d94
ertert2000 <ertert2000@yandex.ru>
2024-09-16 11:34:14 +0300

    5 var ORG_ECM

--- CPP/ORG_ECM/main.cpp
+++ CPP/ORG_ECM/main.cpp
@@ -1,17 +1,47 @@
 #include <iostream>
 #include <bitset>
-#include <conio.h>
+#include <limits>
 
+//Поменять местами заданные пользователем группы рядом стоящих бит, но-мера старших разрядов этих групп и количество бит в группе, вводится с клавиа-туры.
 
 int main()
-{ 
-	//max value = -32768 - 32767
-	short int x = 32767;
+{
+	int initialNumber;
 
-	for (std::size_t i = 0; i < sizeof x; i++)
-		std::cout << " " << std::bitset<8>(reinterpret_cast<unsigned char*>(&x)[i]);
+	int mostSignificantChanged;
+	int mostSignificantDistynation;
+	int qantityBits;
 
-	std::cout << "\n" << sizeof x;
+	while (!(std::cin >> initialNumber))
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		//cout
+	}
+
+	std::bitset<16> bit(initialNumber);
+
+	std::cout << bit << std::endl;
+
+	while (!(std::cin >> mostSignificantChanged) || (mostSignificantChanged < 0) || (mostSignificantChanged > 16))
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+
+	while (!(std::cin >> mostSignificantDistynation) || (mostSignificantDistynation < 0) || (mostSignificantDistynation > 16))
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+
+	while (!(std::cin >> qantityBits))
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+
+	//фориком раскрутить битсет и переставлять элементы
 
 	return 0;
 }
