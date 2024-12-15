d7fabe9d4d2563ab6ce8ad9650cb12c600cd626d
ertert2000 <ertert2000@yandex.ru>
2024-12-15 20:01:07 +0300

    lib10

---
+++ C#/lib10/FootballPlayers.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace lib3_1
+{
+    public class FootballPlayers
+    {
+        public int id {  get; set; }
+        public int price { get; set; }
+        public string name { get; set; }
+        public string firstName { get; set; }
+        public string position { get; set; }
+        public bool mainOrSpare { get; set; }
+
+        public FootballPlayers() {}
+
+        public FootballPlayers(string name, string firstName, string position, int id, int price, bool mainOrSpare)
+        {
+            this.name = name;
+            this.position = position;
+            this.firstName = firstName;
+            this.id = id;
+            this.price = price;
+            this.mainOrSpare = mainOrSpare;
+        }
+
+        public static void deletePlaers(DataGrid fotbal)
+        {
+            try
+            {
+                var selectedPlaers = fotbal.SelectedItem as FootballPlayers;
+
+                if (selectedPlaers == null)
+                    throw new PlayerNotFoundException();
+
+                List<FootballPlayers> newPlayers = fotbal.ItemsSource as List<FootballPlayers>;
+
+                if (newPlayers != null)
+                {
+                    newPlayers.Remove(selectedPlaers);
+
+                    fotbal.ItemsSource = null;
+                    fotbal.ItemsSource = newPlayers;
+                }
+                else
+                    throw new Exception("Failed to retrieve the list of players.");
+            }
+            catch (PlayerNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An unexpected error occurred: {ex.Message}", "Error");
+            }
+        }
+
+        public static void addPlaers(AddPlayerWindow AddPlayerWindow, MainWindow MainWindow)
+        {
+            try
+            {
+                List<FootballPlayers> newPlayers = MainWindow.fotbal.ItemsSource as List<FootballPlayers>;
+                if (newPlayers == null)
+                    newPlayers = new List<FootballPlayers>();
+
+                if (AddPlayerWindow.NameTextBox.Text.Equals("") || AddPlayerWindow.FirstNameTextBox.Text.Equals("") || AddPlayerWindow.PositionTextBox.Text.Equals("") || AddPlayerWindow.PriceTextBox.Text.Equals(""))
+                    throw new PlayerNotFoundException("Text fields are not filled");
+
+                newPlayers.Add(new FootballPlayers(AddPlayerWindow.NameTextBox.Text,
+                                    AddPlayerWindow.FirstNameTextBox.Text,
+                                    AddPlayerWindow.PositionTextBox.Text, 
+                                    1,
+                                    Convert.ToInt32(AddPlayerWindow.PriceTextBox.Text),
+                                    AddPlayerWindow.MainOrSpare.IsChecked ?? false));
+
+                MainWindow.fotbal.ItemsSource = null;
+                MainWindow.fotbal.ItemsSource = newPlayers; 
+            }
+            catch (PlayerNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An unexpected error occurred: {ex.Message}", "Error");
+            }
+        }
+    }
+}
---
+++ C#/lib10/MenuControl.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+
+namespace lib3_1
+{
+    public partial class MainWindow
+    {
+        private void SaveAsButtonClick(object sender, RoutedEventArgs e)
+        {
+            ReportGenerator reportGenerator = new ReportGenerator(fotbal);
+
+            ManualResetEvent dataLoadedEvent = new ManualResetEvent(false);
+            ManualResetEvent xmlProcessedEvent = new ManualResetEvent(false);
+
+            Thread loadXmlThread = new Thread(() =>
+            {
+                try
+                {
+                    reportGenerator.ExportDataGridToPdf("sa.pdf");
+                    dataLoadedEvent.Set();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при загрузке данных: {ex.Message}");
+                }
+            });
+
+            Thread editAndSaveXmlThread = new Thread(() =>
+            {
+                dataLoadedEvent.WaitOne();
+
+                try
+                {
+                    reportGenerator.ExportToHtml("as.html");
+                    xmlProcessedEvent.Set();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при редактировании данных: {ex.Message}");
+                }
+            });
+
+            Thread generateHtmlReportThread = new Thread(() =>
+            {
+                xmlProcessedEvent.WaitOne();
+
+                try
+                {
+                    saveData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при создании отчета: {ex.Message}");
+                }
+            });
+
+            loadXmlThread.Start();
+            editAndSaveXmlThread.Start();
+            generateHtmlReportThread.Start();
+
+
+            SaveSelectedWindow saveSelectedWindow = new SaveSelectedWindow(this);
+            saveSelectedWindow.Show();
+        }
+
+
+        private void OpenButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "CSV Files (*.csv)|*.csv|Text Files (*.txt)|*.txt|Json Files (*.json)|*.json|Xml Files (*.xml)|*.xml",
+                Title = "Выберите файл"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                string format = "";
+                if (System.IO.Path.GetExtension(selectedFilePath).ToLower() == ".csv")
+                    format = "CSV";
+                else if(System.IO.Path.GetExtension(selectedFilePath).ToLower() == ".txt")
+                    format = "TXT";
+                else if(System.IO.Path.GetExtension(selectedFilePath).ToLower() == ".json")
+                    format = "JSON";
+                else if (System.IO.Path.GetExtension(selectedFilePath).ToLower() == ".xml")
+                    format = "XML";
+                else
+                    MessageBox.Show("sd");
+
+                List<FootballPlayers> players = null;
+                if (format == "CSV")
+                    players = DeserializeCsv(selectedFilePath);
+                else if (format == "TXT")
+                    players = DeserializeTxt(selectedFilePath);
+                else if (format == "JSON")
+                    loadData(selectedFilePath);
+                else if(format == "XML")
+                    deserializeXML(selectedFilePath);
+
+
+                if (players != null)
+                {
+                    fotbal.ItemsSource = players;
+                }
+            }
+        }
+    }
+}
